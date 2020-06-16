f1ef68bc5cc3f2319087aaf73aa79b0d32690ef0
steven griffiths <47143897+sgriffi1@users.noreply.github.com>
2020-06-16 10:43:14 +0100

    sync code

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult IndexA()
+        {
+            Product myProduct = new Product 
+            { 
+                ProductID = 1, 
+                Name = "Kayak", 
+                Description = "A boat type thing", 
+                Category = "Watersports", 
+                Price = 275M 
+            };
+
+            ViewBag.StockLevel = 2;
+
+            return View(myProduct);
+        }
+
+        public IActionResult Index()
+        {
+            Product[] array =
+            {
+                new Product {Name = "kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Football", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+            return View(array);
+        }
+
+
+
+
+    }
+}
